1bae30a52da56a63396d7483d685edd31b6c02b5
alexandrecld <alexandre.collado@outlook.fr>
2023-08-22 09:54:21 +0200

    Ajout du responsive

---
+++ lib/models/make_it_responsive.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MakeItResponsive {
+  double minPoint = 640;
+  double maxPoint = 1000;
+
+  ScreenSize getScreenSize(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    if (size.width > minPoint) {
+      return ScreenSize.large;
+    } else if (size.width < maxPoint) {
+      return ScreenSize.small;
+    } else {
+      return ScreenSize.medium;
+    }
+  }
+}
+
+enum ScreenSize {
+  small,
+  medium,
+  large,
+}
